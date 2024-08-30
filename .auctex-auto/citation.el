1ef01c87b61210e3704470f2e4d8fa1f2c95bed5
Baley Eccles <baleycod@gmail.com>
2024-08-30 18:04:35 +1000

    ENG204 Assignment 1.2

---
+++ .auctex-auto/citation.el
@@ -0,0 +1,10 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "citation"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "collimator2024"
+    "Optimization_of_vehicle_suspension_parameters_with_use_of_evolutionary_computation"))
+ '(or :bibtex :latex))
+
