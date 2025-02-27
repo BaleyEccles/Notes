d06ead7ed2d24ee0033f28bc8b3929885ad7e9ad
Baley Eccles <baleycod@gmail.com>
2025-02-27 11:53:37 +1100

    Fix a small bug in KIT107 assignment 0 Q2

--- org-files/KIT107Assignment0Q2.java
+++ org-files/KIT107Assignment0Q2.java
@@ -11,10 +11,24 @@ public class Main {
     System.out.print("Enter another integer: ");
     int int2 = scanner.nextInt();
 
+    // Sort the number by their size
+    // If we dont do this the for loop wont work if we input the biggest number first
+    // We dont need to check the equal case, as there would be no numbers in between them
+    // And that is handled by the else
+    int lowerInt = 0;
+    int upperInt = 0;
+    if (int1 < int2) {
+      lowerInt = int1;
+      upperInt = int2;
+    } else {
+      lowerInt = int2;
+      upperInt = int1;
+    }
+    
     // print the numbers in between the two numbers
-    // NOTE: We must do 'int1 + 1' or else we will also print int1
+    // We must do 'int1 + 1' or else we will also print int1
     // int1 is not in between these two numbers, so it should not be printed
-    for (int i = int1 + 1; i < int2; i++) {
+    for (int i = lowerInt + 1; i < upperInt; i++) {
       System.out.print(i); // print the current number
       System.out.print('\n'); // print a new line
     }
